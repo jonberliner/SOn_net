5e0443837e983ea94333483eba85692bb7d848bd
Jon Berliner <jsberliner@gmail.com>
2024-02-02 13:27:52 -0500

    performance demo added

--- son_net.py
+++ son_net.py
@@ -48,100 +48,116 @@ class SOnLinear(nn.Module):
     def _skew_symmetric(self):
         return self._orth.skew_symmetric()
 
-if __name__ == "__main__":
-    ## DEMO: visualizing activation norms through very deep networks
-    from matplotlib import pyplot as plt
+## NOTE: below moved to notebook
+# if __name__ == "__main__":
+#     ## DEMO: visualizing activation and gradient norms through very deep networks
+#     from matplotlib import pyplot as plt
 
-    DIM = 11
+#     # simulate some data
+#     BATCH_SIZE = 13
+#     INPUT_DIM = DIM
 
-    # assert we have an orthonormal matrix
-    layer = SOnLinear(DIM, bias=True)
-    assert layer.weights.det().isclose(torch.tensor(1.))
-    layer = SOnLinear(DIM, bias=False)
-    assert layer.weights.det().isclose(torch.tensor(1.))
+#     data = torch.randn(BATCH_SIZE, INPUT_DIM)
 
-    # simulate some data
-    BATCH_SIZE = 13
-
-    data = torch.randn(BATCH_SIZE, DIM)
-
-    # ensure we've only rotated the input data, not scaled it
-    layer = SOnLinear(DIM, bias=False)
-    output = layer(data)
-    assert output.norm(dim=1).isclose(data.norm(dim=1)).all()
-
-    layer = SOnLinear(DIM, bias=True)
-    output = layer(data)
-    assert (output - layer.bias).norm(dim=1).isclose(data.norm(dim=1)).all()
-
-    # lets propagate deep!
-    # init models
-    DEPTH = 1001
-    # NONLINEARITY, NLNAME = nn.Identity(), "identity"
-    NONLINEARITY, NLNAME = nn.Tanh(), "tanh"
+#     # assert we have an orthonormal matrix
+#     layer = SOnLinear(DIM, bias=True)
+#     assert layer.weights.det().isclose(torch.tensor(1.))
+#     layer = SOnLinear(DIM, bias=False)
+#     assert layer.weights.det().isclose(torch.tensor(1.))
 
 
-    control, rubiks = [], []
-    for d in range(DEPTH):
-        control += [nn.Linear(DIM, DIM, bias=False), NONLINEARITY]
-        rubiks += [SOnLinear(DIM, bias=False), NONLINEARITY]
-    control = nn.Sequential(*control)
-    rubiks = nn.Sequential(*rubiks)
+#     # ensure we've only rotated the input data, not scaled it
+#     layer = SOnLinear(DIM, bias=False)
+#     output = layer(data)
+#     assert output.norm(dim=1).isclose(data.norm(dim=1)).all()
 
-    # register hooks for inspection
-    activation = {}
+#     layer = SOnLinear(DIM, bias=True)
+#     output = layer(data)
+#     assert (output - layer.bias).norm(dim=1).isclose(data.norm(dim=1)).all()
 
-    def get_activation(name):
-        def hook(model, input, output):
-            activation[name] = output.detach()
-        return hook
-    for d in range(DEPTH):
-        control[d*2].register_forward_hook(get_activation(f"control_{d}"))
-        rubiks[d*2].register_forward_hook(get_activation(f"rubiks_{d}"))
+#     # lets propagate deep!
+#     # init models
+#     DEPTH = 1001
+#     NONLINEARITY, NLNAME = nn.Tanh(), "tanh"
 
-    # init optimizers to inspect gradients
-    control_optim = torch.optim.SGD(
-            params = control.parameters(),
-            lr=0.1,
-        )
-    rubiks_optim = torch.optim.SGD(
-            params = rubiks.parameters(),
-            lr=0.1,
-        )
+#     # make two very deep MLPs
+#     control, rubiks = [], []
+#     for d in range(DEPTH):
+#         control += [nn.Linear(DIM, DIM, bias=False), NONLINEARITY]
+#         rubiks += [SOnLinear(DIM, bias=False), NONLINEARITY]
+#     control = nn.Sequential(*control)
+#     rubiks = nn.Sequential(*rubiks)
 
-    control_out = control(data)
-    rubiks_out = rubiks(data)
+#     # register hooks for inspection
+#     activation = {}
+#     grad = {}
 
-    control_norms, rubiks_norms = [], []
-    for d in range(DEPTH):
-        c = f"control_{d}"
-        r = f"rubiks_{d}"
-        print(f"control act layer {d}: {activation[c].norm(dim=1).mean()}")
-        print(f"rubiks act layer {d}: {activation[r].norm(dim=1).mean()}")
+#     def get_activation(name):
+#         def hook(model, input, output):
+#             activation[name] = output.detach()
+#         return hook
 
-        control_norms.append(activation[c].norm(dim=1).mean())
-        rubiks_norms.append(activation[r].norm(dim=1).mean())
+#     def get_grad(name):
+#         def hook(model, grad_input, grad_output):
+#             grad[name] = grad_output[0].detach()
+#         return hook
 
-    fig, ax = plt.subplots()
-    cl = ax.plot(control_norms, 'b-o', label="control")
-    rl = ax.plot(rubiks_norms, 'g-o', label="rubiks")
-    # leg = ax.legend([cl, rl], ["control", "SO(n)"])
-    ax.set_title(f"output norms for non-linearity {NLNAME}")
-    ax.legend(title="layer type")
-    ax.set_yscale('log')
-    ax.set_xscale('log')
-    ax.set_ylabel("mean activation norm")
-    ax.set_xlabel("layer number")
-    plt.show()
+#     for d in range(DEPTH):
+#         control[d*2].register_forward_hook(get_activation(f"control_{d}"))
+#         rubiks[d*2].register_forward_hook(get_activation(f"rubiks_{d}"))
 
-    # # PRINT DATA NORMS after forward pass
-    # print(f"depth: {DEPTH}, non-linearity: {NONLINEARITY}")
-    # print(f"data norms: {data.norm(dim=1).detach().numpy()}")
-    # print(f"control norms: {control_out.norm(dim=1).detach().numpy()}")
-    # print(f"rubik norms: {rubiks_out.norm(dim=1).detach().numpy()}")
+#     for d in range(DEPTH):
+#         control[d*2].register_backward_hook(get_grad(f"control_{d}"))
+#         rubiks[d*2].register_backward_hook(get_grad(f"rubiks_{d}"))
 
-    loss_fn = nn.MSELoss(reduction="sum")
-    target = torch.randn(BATCH_SIZE, DIM)
+#     # init optimizers to inspect gradients
+#     # forward and backward pass through the network
+#     control_out = control(data)
+#     rubiks_out = rubiks(data)
 
-    control_losses = loss_fn(control_out, target)
-    rubiks_losses = loss_fn(rubiks_out, target)
+#     closs = nn.functional.mse_loss(control_out, data)
+#     rloss = nn.functional.mse_loss(rubiks_out, data)
+
+#     closs.backward()
+#     rloss.backward()
+
+#     control_norms, rubiks_norms = [], []
+#     control_grad_norms, rubiks_grad_norms = [], []
+#     for d in range(DEPTH):
+#         c = f"control_{d}"
+#         r = f"rubiks_{d}"
+
+#         control_norms.append(activation[c].norm(dim=1).mean())
+#         rubiks_norms.append(activation[r].norm(dim=1).mean())
+
+#         control_grad_norms.append(grad[c].norm(dim=1).mean())
+#         rubiks_grad_norms.append(grad[r].norm(dim=1).mean())
+
+#     # plot our results
+#     fig, axes = plt.subplots(1, 2)
+
+#     # activations
+#     ax = axes[0]
+#     ax.set_title(f'activation norms for {NLNAME}')
+#     cl = ax.plot(control_norms, 'b.', label="control")
+#     rl = ax.plot(rubiks_norms, 'g.', label="rubiks")
+#     ax.legend(title="layer type")
+#     ax.set_xscale('log')
+#     # ax.set_yscale('log')
+#     ax.set_ylabel("mean activation norm")
+#     ax.set_xlabel("layer number")
+
+#     # grads
+#     ax = axes[1]
+#     ax.set_title(f'gradient norms for {NLNAME}')
+#     cl = ax.plot(control_grad_norms, 'b.', label="control")
+#     rl = ax.plot(rubiks_grad_norms, 'g.', label="rubiks")
+#     ax.legend(title="layer type")
+#     ax.set_xscale('log')
+#     # ax.set_yscale('log')
+#     ax.set_ylabel("mean gradient norm")
+#     ax.set_xlabel("layer number")
+
+#     plt.show()
+
+#     # activation and gradient norms barely shrink in the SO(n)/Rubik's case!
